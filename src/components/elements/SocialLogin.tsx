00c3da376ab28f3a2144626b08d483f7c968d88a
Bird Hills Technology <birdhillstechnology@gmail.com>
2025-05-15 10:55:57 +0600

    icon change

--- src/components/elements/SocialLogin.tsx
+++ src/components/elements/SocialLogin.tsx
@@ -1,18 +1,18 @@
 'use client'
-import Image from 'next/image';
-import facebookSvg from '../../images/facebook.svg';
-import googleSvg from '../../images/google.svg';
+import { facebookIcon, googleIcon } from '@/images/icons'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const loginSocials = [
 	{
 		name: 'Continue with Facebook',
-		href: '#',
-		icon: facebookSvg,
+		href: '#' as const,
+		icon: facebookIcon,
 	},
 	{
 		name: 'Continue with Google',
-		href: '#',
-		icon: googleSvg,
+		href: '#' as const,
+		icon: googleIcon,
 	},
 ]
 
@@ -22,16 +22,16 @@ export default function SocialLogin() {
 			{/* Socials links */}
 			<div className="grid gap-3">
 				{loginSocials.map((item, index) => (
-					<a
+					<Link
 						key={index}
 						href={item.href}
 						className="flex w-full transform rounded-lg bg-primary-50 px-4 py-3 transition-transform hover:translate-y-[-2px] dark:bg-neutral-800 sm:px-6"
 					>
-						<Image className="flex-shrink-0" src={item.icon} alt={item.name} />
+						<figure className="flex-shrink-0 text-3xl">{item.icon}</figure>
 						<h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
 							{item.name}
 						</h3>
-					</a>
+					</Link>
 				))}
 			</div>
 		</>
